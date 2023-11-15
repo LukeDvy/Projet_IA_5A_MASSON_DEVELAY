3e69afb03a7ccde8f9665123e0aac101261a4a92
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-11-15 15:06:24 +0100

    Création fichiers csv de données par fusion

--- archive/lecture.py
+++ archive/lecture.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
-df = pd.read_csv("Users.csv")
+users = pd.read_csv("Users.csv",low_memory=False)
+ratings=pd.read_csv("Ratings.csv",low_memory=False)
+books=pd.read_csv("Books.csv",low_memory=False)
 
-print(df)
+books = books.drop(columns=["Image-URL-S","Image-URL-M","Image-URL-L"])
+print(users)
+
+users=users.dropna()
+ratings=ratings.dropna()
+books=books.dropna()
+
+df_final=pd.merge(books, ratings, on="ISBN", how="inner")
+df_final=pd.merge(df_final, users, on="User-ID", how="inner")
+
+
+print(df_final.columns)
+print(df_final)
+
+df_final.to_csv('datas.csv', index=False)
