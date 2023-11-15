aa516a3cb429de66cf6a6d7f8eeaa97ee105fdaa
Luke <65294756+LukeDvy@users.noreply.github.com>
2023-11-15 16:11:16 +0100

    Add drop (duplicates, ...)

--- archive/lecture.py
+++ archive/lecture.py
@@ -8,14 +8,24 @@ books = books.drop(columns=["Image-URL-S","Image-URL-M","Image-URL-L"])
 print(users)
 
 users=users.dropna()
+users=users.drop_duplicates()
+
 ratings=ratings.dropna()
+ratings=ratings.drop_duplicates(subset=["User-ID","ISBN"], keep="last") #verifie qu'un lecteur a noté une seule fois un livre
+
 books=books.dropna()
+books=books.drop_duplicates()
+
 
 df_final=pd.merge(books, ratings, on="ISBN", how="inner")
 df_final=pd.merge(df_final, users, on="User-ID", how="inner")
+print(df_final)
 
+#df_final=df_final.drop_duplicates(subset=['Book-Title', 'Book-Author', 'Year-Of-Publication','Publisher'], keep="last"), nous avons remarqué qu'il y a des livres en doublons, publié par plusieurs publisher, cependant ce n'est pas un réelle intérêt à supprimer ces doublons
 
 print(df_final.columns)
 print(df_final)
+
+#print(df_final[df_final["Book-Title"]=="The Way Things Work: An Illustrated Encyclopedia of Technology"])
 
 df_final.to_csv('datas.csv', index=False)
